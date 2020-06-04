af0a26b13dcb093df95bae729f31f3556549a123
shivakumar-insane <shiva@insaneanalytics.com>
2020-06-04 13:01:34 +0530

    AddedTabs

---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-tabs content-class="mt-3">
+      <b-tab title="Home" active>
+        <p>I'm the first tab</p>
+      </b-tab>
+      <b-tab title="Login">
+        <p>I'm the Login tab</p>
+      </b-tab>
+      <b-tab title="SignUp">
+        <p>I'm a Signup Tab!</p>
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
